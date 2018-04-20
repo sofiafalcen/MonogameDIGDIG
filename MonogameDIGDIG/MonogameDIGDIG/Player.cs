33fc9cc78d9aae742b47937a7e1e9729bd082e47
Wilma Berglund <wilma.berglund3@learnet.se>
2018-04-20 13:43:26 +0200

    Movement (position and scale)
    Går nu att flytta med piltangenterna. Är nere vid botten och mindre också.

--- MonogameDIGDIG/MonogameDIGDIG/Player.cs
+++ MonogameDIGDIG/MonogameDIGDIG/Player.cs
@@ -25,11 +25,11 @@ namespace MonogameDIGDIG
         public Player (Texture2D playerTexture)
         {
             triangleTexture = playerTexture;
-            position = new Vector2(100, 100);
+            position = new Vector2(100, 400);
             moveDir = new Vector2(1, 1);
             speed = 300;
             rotation = 0;
-            scale = new Vector2(1, 1);
+            scale = new Vector2(0.12f, 0.2f);
             triangleColor = Color.White;
             offset = (triangleTexture.Bounds.Size.ToVector2() / 2.0f) * scale;
             triangleRectangle = new Rectangle((position - offset).ToPoint(), (triangleTexture.Bounds.Size.ToVector2() * scale).ToPoint());
@@ -38,33 +38,27 @@ namespace MonogameDIGDIG
         public void Update(GameTime gameTime)
         {
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            MouseState mouseState = Mouse.GetState();
-            Vector2 mousePos = mouseState.Position.ToVector2();
-            moveDir = mousePos - position;
-            float pixelsToMove = speed * deltaTime;
+            KeyboardState keyboardState = Keyboard.GetState();
+            moveDir = new Vector2();
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                moveDir.X = 1;
+            }
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                moveDir.X = -1;
+            }
             if (moveDir != Vector2.Zero)
             {
                 moveDir.Normalize();
-                rotation = (float)Math.Atan2(moveDir.Y, moveDir.X);
-                if (Vector2.Distance(position, mousePos) < pixelsToMove)
-                {
-                    position = mousePos;
-                }
-                else
-                {
-                    position += moveDir * pixelsToMove;
-                }
-                triangleRectangle.Location = (position - offset).ToPoint();
+
+                position += moveDir * speed * deltaTime;
             }
-
-
-            triangleColor = Color.White;
-
         }
         
         public void Draw(SpriteBatch spriteBatch)
         {
-        
+
             spriteBatch.Draw(triangleTexture, position, null, triangleColor, rotation, offset, scale, SpriteEffects.None, 0);
 
         }
